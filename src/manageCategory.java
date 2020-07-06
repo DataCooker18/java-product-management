04b98d5530af8f003bb07bd9e07de321cce2dd98
Simba <leaping96@ajou.ac.kr>
2020-07-06 22:41:45 +0900

    categorySet생성자추가

--- src/manageCategory.java
+++ src/manageCategory.java
@@ -4,8 +4,19 @@ public class manageCategory {
 	
 	//인자로 들어온 분류 한개에 대해서 하위분류를 리턴하는 함수
 	public String[] showCategory(String category) {
+		
+		String[] result = new String[999];
+		
 		//인자가 없으면, 대분류 리스트 리턴
+		if(category=="") {
+			
+		}
 		
 		//인자가 있으면, 하위분류리스트 리턴
+		else {
+			
+		}
+			
+		return null;
 	}
 }
