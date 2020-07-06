115fe4f141d346e03e1dae6892a5548b148c55db
DataCooker18 <35443162+DataCooker18@users.noreply.github.com>
2020-07-06 22:45:57 +0900

    product 생성자

--- src/product.java
+++ src/product.java
@@ -4,4 +4,12 @@ public class product
     public String productName;
     public String productSummary;
     public int productPrice;
+
+    public product(int productCode, String productName, String productSummary, int productPrice)
+    {
+        this.productCode = productCode;
+        this.productName = productName;
+        this.productSummary = productSummary;
+        this.productPrice = productPrice;
+    }
 }
